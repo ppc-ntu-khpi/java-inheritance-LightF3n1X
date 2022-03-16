f0f832abe19b7477285c8b63e14b0eeefaf129b4
F3n1X <54713542+LightF3n1X@users.noreply.github.com>
2022-03-16 16:00:51 +0200

    Update Octofregoton.java

--- src/domain/Octofregoton.java
+++ src/domain/Octofregoton.java
@@ -1,51 +1,128 @@
 package domain;
 
+
+ /**
+ * The class Octofregoton extends predator
+ */ 
 public class Octofregoton extends Predator {
 
     private String clasification;
 
-    public Octofregoton(String name, int age, int weight, int height, String clasification) {
+
+/** 
+ *
+ * Octofregoton
+ *
+ * @param name  the name
+ * @param age  the age
+ * @param weight  the weight
+ * @param height  the height
+ * @param clasification  the clasification
+ * 
+ */
+    public Octofregoton(String name, int age, int weight, int height, String clasification) { 
+
      this.name=name;
      this.age=age;
      this.weight=weight;
      this.clasification=clasification;
     }
 
-    public Octofregoton() {
+
+/** 
+ *
+ * Octofregoton
+ *
+ * 
+ */
+    public Octofregoton() { 
+
         this("Blepik", 658, 14561, 3,  "just an avarage Octofregoton");
     }
 
-    public Octofregoton(String name) {
+
+/** 
+ *
+ * Octofregoton
+ *
+ * @param name  the name
+ * @return public
+ */
+    public Octofregoton(String name) { 
+
         this(name, 658, 14586, 3,  "just an avarage Octofregoton" );
     }
 
         
-    public void playwithhumans() {
+
+/** 
+ *
+ * Playwithhumans
+ *
+ */
+    public void playwithhumans() { 
+
         System.out.println("Octofregaton playing with humans...");
     }
 
     @Override
-    public void hunt() {
+
+/** 
+ *
+ * Hunt
+ *
+ */
+    public void hunt() { 
+
         System.out.println("Octofregoton hunting humans..."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
     }
 
     @Override
-    public String toString() {
+
+/** 
+ *
+ * To string
+ *
+ * @return String
+ */
+    public String toString() { 
+
         return super.toString()+"\nClassification:\t"+this.clasification+"\n\nThis is Octofregoton!";
     }
 
     @Override
-    public void drink() {
+
+/** 
+ *
+ * Drink
+ *
+ */
+    public void drink() { 
+
         System.out.println("Octofregoton drinking blood..."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
     }
 
     @Override
-    public void speak() {
+
+/** 
+ *
+ * Speak
+ *
+ */
+    public void speak() { 
+
         System.out.println("Octofregoton Grrrrrrr..."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
     }
 
     @Override
-    public void eat() {
+
+/** 
+ *
+ * Eat
+ *
+ */
+    public void eat() { 
+
         System.out.println("octofregoton eating humans...."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
     }
     
